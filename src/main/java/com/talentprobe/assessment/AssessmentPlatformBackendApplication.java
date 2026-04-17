04b6e548ce85e54a54984ef0844d484e07631442
ariane05149 <arianeabizera6@gmail.com>
2026-04-17 09:11:15 +0200

    Initial backend folder  structure

---
+++ src/main/java/com/talentprobe/assessment/AssessmentPlatformBackendApplication.java
@@ -0,0 +1,13 @@
+package com.talentprobe.assessment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssessmentPlatformBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AssessmentPlatformBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/talentprobe/assessment/AssessmentPlatformBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.talentprobe.assessment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssessmentPlatformBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
