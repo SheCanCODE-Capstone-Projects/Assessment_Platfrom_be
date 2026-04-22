bdbaec86912e3f219222d0880be2ed3b9ab8ec8a
ariane05149 <arianeabizera6@gmail.com>
2026-04-22 23:37:03 +0200

    Dto implementation

--- src/main/java/com/talentprobe/assessment/dto/UserDto.java
+++ src/main/java/com/talentprobe/assessment/dto/UserDto.java
@@ -1,4 +1,11 @@
 package com.talentprobe.assessment.dto;
 
+import lombok.Data;
+
+@Data
 public class UserDto {
+
+    private String name;
+    private String email;
+    private String password;
 }
