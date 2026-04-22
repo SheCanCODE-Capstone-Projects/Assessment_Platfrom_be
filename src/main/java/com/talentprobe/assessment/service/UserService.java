bd9ca94fb7b7b64e8814d24ca2e09bb43230f14c
ariane05149 <arianeabizera6@gmail.com>
2026-04-22 23:41:48 +0200

    implemented the methods : post , get all and get user by id

--- src/main/java/com/talentprobe/assessment/service/UserService.java
+++ src/main/java/com/talentprobe/assessment/service/UserService.java
@@ -1,4 +1,44 @@
 package com.talentprobe.assessment.service;
 
+import com.talentprobe.assessment.dto.UserDto;
+import com.talentprobe.assessment.entity.User;
+import com.talentprobe.assessment.enums.Role;
+import com.talentprobe.assessment.enums.Status;
+import com.talentprobe.assessment.mapper.UserMapper;
+import com.talentprobe.assessment.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
 public class UserService {
+
+    private final UserRepository userRepository;
+
+    private final UserMapper userMapper;
+
+    public User createUser(UserDto dto) {
+
+        User user = userMapper.toEntity(dto);
+
+        user.setStatus(Status.ACTIVE);
+        user.setCreatedAt(LocalDateTime.now());
+
+        user.setRole(Role.CANDIDATE);
+
+        return userRepository.save(user);
+    }
+
+    public List<User> getAllUsers() {
+
+        return userRepository.findAll();
+    }
+
+    public User getUserById(Long id) {
+        return userRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+    }
 }
