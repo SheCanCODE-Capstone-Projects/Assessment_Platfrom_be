559aa05a68912b97cb80a8f33e4fbf81c270c95b
ariane05149 <arianeabizera6@gmail.com>
2026-04-22 23:43:22 +0200

    added both ends points from implemented methods in service class

--- src/main/java/com/talentprobe/assessment/controller/UserController.java
+++ src/main/java/com/talentprobe/assessment/controller/UserController.java
@@ -1,4 +1,33 @@
 package com.talentprobe.assessment.controller;
 
+import com.talentprobe.assessment.dto.UserDto;
+import com.talentprobe.assessment.entity.User;
+import com.talentprobe.assessment.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+@RequiredArgsConstructor
 public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping
+    public User createUser(@RequestBody UserDto dto) {
+
+        return userService.createUser(dto);
+    }
+
+    @GetMapping
+    public List<User> getAllUsers() {
+
+        return userService.getAllUsers();
+    }
+    @GetMapping("/{id}")
+    public User getUserById(@PathVariable Long id) {
+        return userService.getUserById(id);
+    }
 }
