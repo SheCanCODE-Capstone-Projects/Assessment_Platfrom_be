a5487e043ddc08388e5a74d08bdd68b6f06439c4
ariane05149 <arianeabizera6@gmail.com>
2026-04-22 23:37:42 +0200

    Mapping by mapstruct type

--- src/main/java/com/talentprobe/assessment/mapper/UserMapper.java
+++ src/main/java/com/talentprobe/assessment/mapper/UserMapper.java
@@ -1,4 +1,13 @@
 package com.talentprobe.assessment.mapper;
 
-public class UserMapper {
+import com.talentprobe.assessment.dto.UserDto;
+import com.talentprobe.assessment.entity.User;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface UserMapper {
+
+    User toEntity(UserDto dto);
+
+    UserDto toDto(User user);
 }
