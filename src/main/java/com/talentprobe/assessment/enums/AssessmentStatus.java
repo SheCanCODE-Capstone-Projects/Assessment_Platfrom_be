b7dfaf8f869d6f882deddd27af8b5885b0545ab3
jeannette uwimbabazi <janetmbabazi885@gmail.com>
2026-04-27 15:04:44 +0200

    feat: add Difficulty, Language, AssessmentStatus enums

---
+++ src/main/java/com/talentprobe/assessment/enums/AssessmentStatus.java
@@ -0,0 +1,6 @@
+package com.talentprobe.assessment.enums;
+
+public enum AssessmentStatus {
+    ACTIVE,
+    INACTIVE
+}
---
+++ src/main/java/com/talentprobe/assessment/enums/Language.java
@@ -0,0 +1,21 @@
+package com.talentprobe.assessment.enums;
+
+public enum Language {
+    JAVASCRIPT,
+    TYPESCRIPT,
+    PYTHON,
+    JAVA,
+    CPP,
+    C,
+    CSHARP,
+    GO,
+    RUST,
+    KOTLIN,
+    SWIFT,
+    RUBY,
+    PHP,
+    SCALA,
+    R,
+    DART,
+    SQL
+}
